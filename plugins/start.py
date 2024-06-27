1d2c87ee38b240e870be971456f0e52187a191aa
sayantanmandal009 <157054653+sayantanmandal009@users.noreply.github.com>
2024-06-27 23:21:06 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -85,7 +85,7 @@ async def start_command(client: Client, message: Message):
             except:
                 pass
 
-        k = await message.reply_text("<b>❗️ <u>baka!</u> ❗️</b>\n\n<b>This video / file will be deleted in 10 minutes (Due to copyright issues).\n\n📌 Please forward this video / file to somewhere else and start downloading there.</b>")
+        k = await message.reply_text("<b>❗️ <u>WARNING!</u> ❗️</b>\n\n<b>This video / file will be deleted in 10 minutes (Due to copyright issues).\n\n📌 Please forward this video / file to somewhere else and start downloading there.</b>")
         await asyncio.sleep(SECONDS)
 
         for data in CodeXBotz:
