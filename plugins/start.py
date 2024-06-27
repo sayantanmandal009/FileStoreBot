cf16a412185620a169d9f125b35a7396c24d27b9
sayantanmandal009 <157054653+sayantanmandal009@users.noreply.github.com>
2024-06-27 23:10:38 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,7 @@ async def start_command(client: Client, message: Message):
         k = await message.reply_text("<b>❗️ <u>baka!</u> ❗️</b>\n\n<b>This video / file will be deleted in 10 minutes (Due to copyright issues).\n\n📌 Please forward this video / file to somewhere else and start downloading there.</b>")
         await asyncio.sleep(SECONDS)
 
-        for data in Codeflix:
+        for data in CodeXBotz:
             try:
                 await data.delete()
                 await k.edit_text("<b>Your video / file is successfully deleted !</b>")
