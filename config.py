1c719593752c9f9d46f00db4182f1592a49e557a
sayantanmandal009 <157054653+sayantanmandal009@users.noreply.github.com>
2024-06-28 08:13:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002154829330"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "778976952"))
 
 #Port
-PORT = os.environ.get("PORT", "8080")
+PORT = os.environ.get("PORT", "8030")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
